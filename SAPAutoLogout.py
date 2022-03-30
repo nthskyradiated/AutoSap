4235a55d464dccc580a4fbe62165ee9965f0dc57
nthskyradiated <71885584+nthskyradiated@users.noreply.github.com>
2022-03-30 09:08:10 +0400

    added .py
    added .py codes

---
+++ SAPAutoLogout.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep as sl
+from random import randint
+import os 
+
+username=os.environ.get("USER_NAME")
+
+password=os.environ.get("PASSWORD") 
+
+sapurl=os.environ.get("SAP")
+
+x = randint(1,600)
+sl(x)
+
+driver = webdriver.Chrome()
+driver.get(sapurl)
+logon_field = driver.find_element_by_id("logonuidfield")
+logon_field.send_keys(username)
+pass_field = driver.find_element_by_id("logonpassfield")
+pass_field.send_keys(password)
+logon_button = driver.find_element_by_name("uidPasswordLogon")
+logon_button.click()
+
+driver.implicitly_wait(30)
+
+att_click = driver.find_element_by_css_selector("#__tile3")
+att_click.send_keys(Keys.SPACE)
+
+
+att_click = driver.find_element_by_css_selector("#__tile21")
+
+att_click.click()
+sl(15)
+driver.quit()
+
+
+  
